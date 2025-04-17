2af50577f596c3ff5c8841c28245f02f522d1ca7
David wacholder <185147826+David-wacholder@users.noreply.github.com>
2025-04-17 21:24:21 +0300

    Update smartalk_filter.rb

--- _plugins/smartalk_filter.rb
+++ _plugins/smartalk_filter.rb
@@ -1,10 +1,37 @@
+require "nokogiri"
+
 module Jekyll
   module SmartalkFilter
     def highlight_smartalk(input)
-      # מחפש את המילה Smartalk ומחליף אותה, כולל אחרי תווים כמו פסיק ונקודה
-      input.gsub(/Smartalk(?=[\s\.,;!\?:]|$)/, '<span class="smartalk-word"><span class="smartalk-smart">Smar</span><span class="smartalk-talk">talk</span></span>')
+      input.gsub(/Smartalk(?=[\s\.,;!\?:]|$)/i, '<span class="smartalk-word"><span class="smartalk-smart">Smar</span><span class="smartalk-talk">talk</span></span>')
+    end
+
+    def smartalkify_html(html)
+      doc = Nokogiri::HTML::DocumentFragment.parse(html)
+
+      doc.traverse do |node|
+        if node.text? && !ancestor_is_code_or_style?(node)
+          node.replace(highlight_smartalk(node.text))
+        end
+      end
+
+      doc.to_html
+    end
+
+    private
+
+    def ancestor_is_code_or_style?(node)
+      node.ancestors.any? { |ancestor| %w[script style code pre].include?(ancestor.name.downcase) }
     end
   end
 end
 
 Liquid::Template.register_filter(Jekyll::SmartalkFilter)
+
+# הפעלת הפילטר אוטומטית על כל הדפים והפוסטים
+Jekyll::Hooks.register [:pages, :posts], :post_render do |doc|
+  if doc.output_ext == ".html"
+    filter = Jekyll::SmartalkFilter.new
+    doc.output = filter.smartalkify_html(doc.output)
+  end
+end
